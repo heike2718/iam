e1bec0871fcb5fea8e2a18ecbfe3371bdfef32e7
Heike Winkelvoß <public@egladil.de>
2020-09-12 19:11:03 +0200

    docs verschoben, neue versionsnummer

--- frontend/authprovider-ws/apps/auth-app/src/environments/environment.prod.ts
+++ frontend/authprovider-ws/apps/auth-app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	version: '5.1.2',
+	version: '7.1.1',
 	envName: 'Production',
 	apiUrl: 'https://opa-wetterwachs.de/authprovider',
 	profilUrl: 'https://opa-wetterwachs.de/profil-app',
