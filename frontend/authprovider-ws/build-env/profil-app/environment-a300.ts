fb968199d91c9c391e23f9e788fa92ad578c1152
Heike Winkelvoß <public@egladil.de>
2020-10-17 21:13:41 +0200

    build config for test server

---
+++ frontend/authprovider-ws/build-env/profil-app/environment-a300.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  version: '7.1.1',
+  envName: 'a300',
+  apiUrl: 'http://a300/profil-api',
+  assetsUrl: 'http://a300/profil-app/assets',
+  loginRedirectUrl: 'http://a300/profil-app',
+  consoleLogActive: true,
+  serverLogActive: true,
+  loglevel: 1
+};
