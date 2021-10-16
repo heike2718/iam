8c68a1c3f49cc8bba5e4c3ec24061c07157959a9
Heike Winkelvoß <public@egladil.de>
2021-10-16 14:18:49 +0200

    not authorized when no XSRF-TOKEN header

---
+++ frontend/authprovider-ws/libs/common-cookies/src/lib/common-cookies.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+@NgModule({
+	imports: [
+        CookieService
+	],
+	declarations: [
+	],
+	exports: [
+	]
+})
+export class CommonCookiesModule { }
