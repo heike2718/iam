de83bf3487f30861e18723a74b45e8c75e916bc9
Heike Winkelvoß <public@egladil.de>
2024-06-15 18:53:58 +0200

    I0034: das nx-tooling ist schrott

--- frontend/authadmin-ws/libs/shared/config/src/lib/config/configuration.ts
+++
@@ -1,9 +0,0 @@
-
-export class Configuration {
-    constructor(
-        public baseUrl: string,
-        public assetsPath: string,
-        public clientId: string,
-        public production: boolean
-    ) { }
-};
