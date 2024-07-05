644c0f5ce6880548fb917404b3360feecf6cdc3d
Heike Winkelvoß <public@egladil.de>
2024-07-05 09:17:53 +0200

    I0034: statische Tabelle zur Anzeige des benutzer-basket

--- frontend/bv-admin-app/src/app/benutzer/feature/benutzer-basket/benutzer-basket.component.ts
+++ frontend/bv-admin-app/src/app/benutzer/feature/benutzer-basket/benutzer-basket.component.ts
@@ -4,27 +4,38 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatTableModule } from "@angular/material/table";
 import { BenutzerFacade } from "@bv-admin-app/benutzer/api";
 
+const UUID = 'uuid';
+const EMAIL = 'email';
+const NACHNAME = 'nachname';
+const VORNAME = 'vorname';
+const DATE_MODIFIED = 'dateModified';
+const ROLLE = 'rolle';
+
 
 @Component({
     selector: 'bv-users',
     standalone: true,
     imports: [
-      CommonModule,
-      NgIf,
-      NgFor,
-      AsyncPipe,
-      MatTableModule,
-      MatButtonModule
+        CommonModule,
+        NgIf,
+        NgFor,
+        AsyncPipe,
+        MatTableModule,
+        MatButtonModule
     ],
     templateUrl: './benutzer-basket.component.html',
     styleUrls: ['./benutzer-basket.component.scss'],
-  })
+})
 export class BenutzerBasketComponent {
 
     benutzerFacade = inject(BenutzerFacade);
 
     reset(): void {
         this.benutzerFacade.resetBenutzerBasket();
+    }
+
+    getDisplayedColumns(): string[] {
+        return [UUID, EMAIL, NACHNAME, VORNAME, DATE_MODIFIED, ROLLE];
     }
 
 }
